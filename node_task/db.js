6b4987fd1a849f14410e93bb4909eba418b88e61
TimOn <via-lactea757@yandex.ru>
2021-03-17 10:40:44 +0500

    add project

--- node_task/db.js
+++ node_task/db.js
@@ -1,7 +1,14 @@
 const db = {
   products: [
-    { id_product: 1, product_name: "Ноутбук", price: 45600 },
-    { id_product: 2, product_name: "Мышка", price: 1000 }
+    { id_product: 1, img: './img/product1.jpg', product_name: "Mango People T-shirt", price: 22, color: 'white', size: 'S' },
+    { id_product: 2, img: './img/product2.jpg', product_name: "Mango People Blouse", price: 32, color: 'red', size: 'M' },
+    { id_product: 3, img: './img/product3.jpg', product_name: "Mango People Jacket", price: 52, color: 'blue', size: 'XS' },
+    { id_product: 4, img: './img/product4.jpg', product_name: "Mango People Top", price: 35, color: 'flower', size: 'S' },
+    { id_product: 5, img: './img/product5.jpg', product_name: "Mango People Blouse", price: 41, color: 'white/black', size: 'L' },
+    { id_product: 6, img: './img/product6.jpg', product_name: "Mango People Hat", price: 19,
+      color: 'blue', size: 'M' },
+    { id_product: 7, img: './img/product7.jpg', product_name: "Mango People Trousers", price: 58, color: 'beige', size: 'XL' },
+    { id_product: 8, img: './img/product8.jpg', product_name: "Mango People Shorts", price: 46, color: 'beige', size: 'S' }
   ],
 };
 
